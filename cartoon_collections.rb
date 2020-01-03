9be13d89232325029fec3ed6296b2f27cb8cd05d
Samanta Chowdhury <csamanta95@gmail.com>
2020-01-03 04:59:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
   end
+end
 roll_call_dwarves(dwarves)
 
 def summon_captain_planet(planeteer_calls)# code an argument here
